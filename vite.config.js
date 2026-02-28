99dfe68d0f214796d3d64e7a7773b5c8bcbf2171
KolynHJ <112416424+KLN1915@users.noreply.github.com>
2026-02-27 21:12:54 -0500

    vite config cambiado para que reconozca js en produccion

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,29 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                //CARPETAS JS
+                //clients
+                'resources/js/clients/createClient.js',
+                'resources/js/clients/editClient.js',
+                'resources/js/clients/showClient.js',
+                //contracts
+                'resources/js/contracts/changeStateContract.js',
+                'resources/js/contracts/createContract.js',
+                'resources/js/contracts/editContract.js',
+                //debts
+                'resources/js/debts/showDebts.js',
+                //payments
+                'resources/js/payments/makePayment.js',
+                'resources/js/payments/nullPayment.js',
+                'resources/js/payments/showReceipt.js',
+                //settings
+                'resources/js/settings/services/createService.js',
+                'resources/js/settings/services/editService.js',
+                'resources/js/settings/additional-services/createAddService.js',
+                'resources/js/settings/additional-services/editAddService.js',
+                //zones
+                'resources/js/zones/createZone.js',
+                'resources/js/zones/editZone.js',
             ],
             refresh: true,
         }),
